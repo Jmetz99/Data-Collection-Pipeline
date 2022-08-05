90e3710bec60cc5e52b2c2cbb278a1efd62488bf
Jmetz99 <jacobmetz12@gmail.com>
2022-08-05 08:36:55 +0100

    Updates scraper to scroll to next page button and close pop up.

--- scraper.py
+++ scraper.py
@@ -12,29 +12,16 @@ class GorillaMindScraper:
             self.url = 'https://gorillamind.com/collections/all?page=1'
             chrome_options = Options()
             self.driver = webdriver.Chrome(options=chrome_options)
-
-
+            self.open_page()
+            self.get_links()
+            self.scroll_to_next_page_button()
+            self.close_offer()
+            self.go_to_next_page()
             
         def open_page(self):
             self.driver.get(self.url)
 
-        def scroll_page(self):
-            self.open_page()
-            time.sleep(2)
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-        def go_to_next_page(self):
-            self.scroll_page()
-            next_page = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-collection__main"]/div/div[2]/div/div/nav/a')
-            next_page.click()
-        
-        def click_product(self):
-            self.open_page()
-            product = self.driver.find_element(by=By.XPATH, value = '//*[@id="shopify-section-collection__main"]/div/div[1]/div[2]/div[4]/div/div[1]/div[2]/a')
-            product.click()
-        
         def get_links(self):
-            self.open_page()
             prod_container = self.driver.find_element(by=By.XPATH, value = '//*[@id="shopify-section-collection__main"]/div/div[1]/div[2]')
             prod_list = prod_container.find_elements(by=By.XPATH, value = './div')
             link_list = []
@@ -42,12 +29,28 @@ class GorillaMindScraper:
                 a_tag = product.find_element(by=By.TAG_NAME, value='a')
                 link = a_tag.get_attribute('href')
                 link_list.append(link)
+            print(link_list)
             return link_list
+            
 
-gorilla_mind = Scraper()
+        def click_product(self):
+            product = self.driver.find_element(by=By.XPATH, value = '//*[@id="shopify-section-collection__main"]/div/div[1]/div[2]/div[4]/div/div[1]/div[2]/a')
+            product.click()
+            pass
 
-#gorilla_mind_prods = gorilla_mind.get_links()
+        def scroll_to_next_page_button(self):
+            time.sleep(3)
+            next_page = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-collection__main"]/div/div[2]/div/div/nav/a')
+            self.driver.execute_script("arguments[0].scrollIntoView();", next_page)
 
-products = gorilla_mind.get_links()
+        def close_offer(self):
+            time.sleep(4)
+            close_button = self.driver.find_element(by=By.XPATH, value='//*[@id="SMSBump-Modal"]/div/div/button')
+            close_button.click()
 
-print(products)
+        def go_to_next_page(self):
+            next_page = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-collection__main"]/div/div[2]/div/div/nav/a')
+            next_page.click()
+
+gorilla_mind = GorillaMindScraper()
+
