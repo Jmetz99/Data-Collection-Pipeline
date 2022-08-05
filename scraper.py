a072d1a68ec191c5af2eb19bdc25acb1c5c10a25
Jmetz99 <jacobmetz12@gmail.com>
2022-08-05 15:27:09 +0100

    Adds product_id function which strips url to leave name of product as ID

--- scraper.py
+++ scraper.py
@@ -13,6 +13,7 @@ class GorillaMindScraper:
             self.url = 'https://gorillamind.com/collections/all?page=1'
             chrome_options = Options()
             self.driver = webdriver.Chrome(options=chrome_options)
+            self.dict_products = {'Name': [], 'Price': [], 'Description': [], 'Flavours': [], 'Rating': [], 'ID': []}
             
         def open_page(self):
             self.driver.get(self.url)
@@ -50,26 +51,32 @@ class GorillaMindScraper:
         
         def extract_text(self, link):
             self.driver.get(link)
-            dict_products = {'Name': [], 'Price': [], 'Description': [], 'Flavours': [], 'Rating': []}
 
             name = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-product__supplements"]/section[1]/section/div/div/div[2]/div[1]/h1').text
-            dict_products['Name'].append(name)
+            self.dict_products['Name'].append(name)
 
             price = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-product__supplements"]/section[1]/section/div/div/div[2]/div[1]/p').text
-            dict_products['Price'].append(price)
+            self.dict_products['Price'].append(price)
 
             description = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-product__supplements"]/section[2]/div/div/div[1]/div/div[1]/div[1]/span[1]').text
-            dict_products['Description'].append(description)
+            self.dict_products['Description'].append(description)
 
             flavours = self.driver.find_element(by=By.XPATH, value='//*[@id="product_form_4898112667693"]/div[2]/div[1]').text
             flavour_list = list(flavours.splitlines())
             flavour_list.remove('Flavor')
-            dict_products['Flavours'].append(flavour_list)
+            self.dict_products['Flavours'].append(flavour_list)
 
             rating = self.driver.find_element(by=By.XPATH, value='//*[@id="shopify-section-68eb7e26-87f6-4711-8408-2327df293f70"]/section/div/div/div/div/span/div[1]/div/div[1]/span').text
-            dict_products['Rating'].append(rating)
+            self.dict_products['Rating'].append(rating)
 
-            return dict_products
+            return self.dict_products
+
+        def product_id(self, url):
+            id = url.replace('https://gorillamind.com/collections/all-products/products/', '')
+            self.dict_products['ID'].append(id)
+            print(id)
+            return self.dict_products
+
 
         def extract_image(self, link):
             self.driver.get(link)
@@ -81,5 +88,4 @@ class GorillaMindScraper:
 
 
 gorilla_mind = GorillaMindScraper()
-prod_info = gorilla_mind.extract_text("https://gorillamind.com/collections/all/products/gorilla-mode")
-
+gorilla_mind.product_id('https://gorillamind.com/collections/all-products/products/gorilla-mode')
